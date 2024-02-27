364ff5baa6a100b910f85aa8e6398ef2634d17a1
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-02-27 12:28:14 -0500

    establish repo

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import store from './store.js';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>
+);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, './client/index.js'),
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  //   mode: process.env.NODE_ENV,
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'development', template: './index.html' }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    host: 'localhost',
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname),
+      publicPath: '/',
+    },
+    proxy: [
+      {
+        context: ['/api', '/test', '/user'],
+        target: 'http://localhost:3000',
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.mp3$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+};
