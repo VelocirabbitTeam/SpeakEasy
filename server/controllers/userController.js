8abc64a4309c191028e2285b209bcb94dbe9e6a2
Jinseong Nam <jinseongnam@Jinseongs-MacBook-Air.local>
2024-02-28 10:17:13 -0500

    completed schema for database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,3 @@
-// CREATE TABLE IF NOT EXISTS users (
-//     id SERIAL PRIMARY KEY,
-//     username VARCHAR(255) UNIQUE NOT NULL,
-//     email VARCHAR(255) NOT NULL,
-//     password VARCHAR(255) NOT NULL,
-//     created_at TIMESTAMP DEFAULT NOW()
-// );
-
 const User = require('../models/dbModels.js');
 
 const userController = {};
@@ -26,61 +18,6 @@ userController.createUser = async (req, res, next) => {
   } catch (err) {
     return next(err);
   }
-};
-
-userController.verifyUser = (req, res, next) => {
-  const { username, password } = req.body;
-
-  // Get the user from the database
-  const query = `SELECT * FROM users WHERE username = '${username}';`;
-  db.query(query)
-    .then((data) => {
-      const user = data.rows[0];
-
-      // Compare the provided password with the hashed password in the database
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (err) return next(err);
-
-        if (result) {
-          // Passwords match
-          console.log('confirmed');
-          res.locals.user = user;
-          return next();
-        } else {
-          // Passwords don't match
-          return next(new Error('Invalid password'));
-        }
-      });
-    })
-    .catch((err) => next(err));
-};
-
-userController.updateUser = (req, res, next) => {
-  const { username, email, password } = req.body;
-
-  // Hash the password before updating it in the database
-  bcrypt.hash(password, saltRounds, (err, hash) => {
-    if (err) return next(err);
-
-    const query = `UPDATE users SET email = '${email}', password = '${hash}' WHERE username = '${username}';`;
-    db.query(query)
-      .then((data) => {
-        console.log(data);
-        return next();
-      })
-      .catch((err) => next(err));
-  });
-};
-
-userController.deleteUser = (req, res, next) => {
-  const { username } = req.body;
-  const query = `DELETE FROM users WHERE username = '${username}';`;
-  db.query(query)
-    .then((data) => {
-      console.log(data);
-      return next();
-    })
-    .catch((err) => next(err));
 };
 
 userController.getAllUsers = (req, res, next) => {
--- server/models/dbModels.js
+++ server/models/dbModels.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrpytjs');
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
   name: {
@@ -18,6 +21,32 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.pre('save', function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) return next(err);
+
+        this.password = hash;
+
+        return next();
+      });
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (passwordInput) {
+  return await bcrypt.compare(passwordInput, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
 
